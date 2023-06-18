471520a69fb2adb47f001f71ac63d900535cde9d
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-06-18 16:14:19 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+class script(object):
+
+
+JK_DEVELOPR = """<b> {filename}
+
+💝 𝗝𝗼𝗶𝗻 𝗕𝗮𝗰𝗸𝗨𝗽 : <a href=https://t.me/luluofficiall>𝑪𝒍𝒊𝒄𝒌 𝑯𝒆𝒓𝒆</a> 👈
+💝 𝗠𝗼𝘃𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 : <a href=https://t.me/luluofficiall>𝑪𝒍𝒊𝒄𝒌 𝑯𝒆𝒓𝒆</a> 👈</b>"""
