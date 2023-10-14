5a1714a05b3fb2bd2f5412fae93fb9cf1e3b2cbc
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-10-14 15:47:39 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href=https://t.me/Master_Jiraya>🄼𝗮𝘀𝘁𝗲𝗿 𝗝𝗶𝗿𝗮𝘆𝗮</a>,
+            text = f"<b>○ Creator : <a href="https://t.me/Master_Jiraya">🄼𝗮𝘀𝘁𝗲𝗿 𝗝𝗶𝗿𝗮𝘆𝗮</a>,
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
